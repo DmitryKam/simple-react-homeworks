e119349abed9f70400d159f2404cb539e8d65730
DmitryKam <k8741460@gmail.com>
2020-09-24 21:49:07 +0300

    Updates

---
+++ precache-manifest.b400caf19fd3ad6a84bf39b2bd470b02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b13d4445d71c432b81efdd5f116f965",
+    "url": "/simple-react-homeworks/index.html"
+  },
+  {
+    "revision": "f0d77f2ede33579f6b02",
+    "url": "/simple-react-homeworks/static/css/main.d94b5108.chunk.css"
+  },
+  {
+    "revision": "5a5632cd1d10e5f21e4c",
+    "url": "/simple-react-homeworks/static/js/2.83b71092.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-react-homeworks/static/js/2.83b71092.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0d77f2ede33579f6b02",
+    "url": "/simple-react-homeworks/static/js/main.da467471.chunk.js"
+  },
+  {
+    "revision": "35a9557543d5cb14c0c5",
+    "url": "/simple-react-homeworks/static/js/runtime-main.5a451111.js"
+  }
+]);
