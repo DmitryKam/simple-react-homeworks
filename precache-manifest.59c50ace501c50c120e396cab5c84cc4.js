1fe3940d259490bdf104ca9ba93af52f46bea417
DmitryKam <k8741460@gmail.com>
2020-10-13 20:15:38 +0300

    Updates

--- precache-manifest.59c50ace501c50c120e396cab5c84cc4.js
+++ precache-manifest.59c50ace501c50c120e396cab5c84cc4.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "99008d4024d05ee704cc27a16b93f7cb",
+    "revision": "e032b3bb9f3216c94377e778118d4224",
     "url": "/simple-react-homeworks/index.html"
   },
   {
-    "revision": "2b20842b1db601c903c7",
+    "revision": "d0ee4052ab7cb5231790",
     "url": "/simple-react-homeworks/static/css/main.a3d136d8.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/simple-react-homeworks/static/js/2.2d1751dd.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2b20842b1db601c903c7",
-    "url": "/simple-react-homeworks/static/js/main.0415be81.chunk.js"
+    "revision": "d0ee4052ab7cb5231790",
+    "url": "/simple-react-homeworks/static/js/main.dd6a8f90.chunk.js"
   },
   {
     "revision": "35a9557543d5cb14c0c5",
